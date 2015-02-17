01070c85b73c5bc357966a6ddd147c782c7c8cc7
David Little <david.frank.little@gmail.com>
2015-02-17 15:48:05 -0600

    Added code to compile pystan model upon install.

--- setup.py
+++ setup.py
@@ -1,6 +1,32 @@
-from distutils.core import setup
+from  setuptools  import  setup
+from  setuptools.command.install import install as _install
+import pickle, os, datetime
 
-setup(name='pylab_util',version='0.0.1',author='David Little',
+class pystan_compile(_install):
+    def run(self):
+        import pystan
+
+        if os.stat('src/stan/mean.model.stan').st_mtime > \
+            os.stat('src/stan/mean.model.o').st_mtime:
+
+            model = pystan.StanModel(file='src/stan/mean.model.stan')
+            with open('src/stan/mean.model.o','wb') as f: pickle.dump(model,f)
+        
+        if os.stat('src/stan/ind.model.stan').st_mtime > \
+            os.stat('src/stan/ind.model.o').st_mtime:
+
+            model = pystan.StanModel(file='src/stan/ind.model.stan')
+            with open('src/stan/ind.model.o','wb') as f: pickle.dump(model,f)
+
+        _install.run(self)
+
+setup(cmdclass = {'install': pystan_compile},
+      name='pylab_util',version='0.0.2',author='David Little',
       packages=['pylab_util'],
       package_dir={'pylab_util': 'src'},
-      requires=['statsmodels(>=0.5.0)'])
+      setup_requires = ['pystan>=2.5.0'],
+      package_data={'pylab_util': ['stan/*.o']},
+      requires=['statsmodels(>=0.5.0)',
+                'pystan(>=2.5.0)',
+                'patsy(>=0.3.0)',
+                'numpy','pandas'])
