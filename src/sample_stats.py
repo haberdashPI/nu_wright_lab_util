b5de9c91ce4dc84d6cd0a1a271e1f818291d5e58
David Little <david.frank.little@gmail.com>
2015-02-18 20:01:04 -0600

    Created some utilities for statistical analysis given MC samples.

---
+++ src/sample_stats.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def p_value(xs): return 2*np.mean(np.sign(xs) != np.sign(np.mean(xs)))
+
+def ppp(y,model,stats,N=1000):
+    def test(y,y_hat):
+        diffs = y_hat - y
+        fake_diffs = np.random.normal(scale=model.error[y_hat.name],size=len(y_hat))
+
+        return pd.DataFrame([{'real': stat(diffs), 'fake': stat(fake_diffs), 'type': name}
+                             for name, stat in stats.iteritems()])
+
+    samples = np.random.randint(model.y_hat.shape[0],size=N)
+    return pd.concat([test(y,model.y_hat.iloc[i,:]) for i in samples])
+
