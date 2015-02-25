300748ba7fbf34aae6a68f32fdfbba29e97dbf27
David Little <david.frank.little@gmail.com>
2015-02-25 00:41:27 -0500

    Fixed whitespaces.

--- src/sample_stats.py
+++ src/sample_stats.py
@@ -6,9 +6,12 @@ def p_value(xs): return 2*np.mean(np.sign(xs) != np.sign(np.mean(xs)))
 def ppp(y,model,stats,N=1000):
     def test(y,y_hat):
         diffs = y_hat - y
-        fake_diffs = np.random.normal(scale=model.error[y_hat.name],size=len(y_hat))
+        fake_diffs = np.random.normal(scale=model.error[y_hat.name],
+                                      size=len(y_hat))
 
-        return pd.DataFrame([{'real': stat(diffs), 'fake': stat(fake_diffs), 'type': name}
+        return pd.DataFrame([{'real': stat(diffs),
+                              'fake': stat(fake_diffs),
+                              'type': name}
                              for name, stat in stats.iteritems()])
 
     samples = np.random.randint(model.y_hat.shape[0],size=N)
