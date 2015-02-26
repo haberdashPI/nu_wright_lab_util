6396be74704c9c2bba645c97cbb83019c85fb81e
David Little <david.frank.little@gmail.com>
2015-02-25 23:10:27 -0500

    ppp now works with either all or partial samples.

--- src/sample_stats.py
+++ src/sample_stats.py
@@ -3,17 +3,18 @@ import pandas as pd
 
 def p_value(xs): return 2*np.mean(np.sign(xs) != np.sign(np.mean(xs)))
 
-def ppp(y,model,stats,N=1000):
-    def test(y,y_hat):
+def ppp(y,y_hats,error,stats,N=1000):
+    def test(y,y_hat,error):
         diffs = y_hat - y
-        fake_diffs = np.random.normal(scale=model.error[y_hat.name],
-                                      size=len(y_hat))
+        fake_diffs = np.random.normal(scale=error,size=len(y_hat))
 
-        return pd.DataFrame([{'real': stat(diffs),
-                              'fake': stat(fake_diffs),
+        return pd.DataFrame([{'real': stat(diffs),'fake': stat(fake_diffs),
                               'type': name}
-                             for name, stat in stats.iteritems()])
+                              for name, stat in stats.iteritems()])
 
-    samples = np.random.randint(model.y_hat.shape[0],size=N)
-    return pd.concat([test(y,model.y_hat.iloc[i,:]) for i in samples])
-
+    if N is None:
+        return pd.concat([test(y,y_hats[i,:],error[i])
+                          for i in range(len(error))])
+    else:
+        samples = np.random.randint(y_hats.shape[0],size=N)
+        return pd.concat([test(y,y_hats[i,:],error[i]) for i in samples])
