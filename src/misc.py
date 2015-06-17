b120807622ca4961573460fe9a918d9fcf9804da
David Little <david.frank.little@gmail.com>
2015-06-17 14:06:52 -0500

    Created set of miscellaneous useful functions.

---
+++ src/misc.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def unique_rows(a):
+    order = np.lexsort(a.T)
+    a = a[order]
+    diff = np.diff(a, axis=0)
+    ui = np.ones(len(a), 'bool')
+    ui[1:] = (diff != 0).any(axis=1)
+    return a[ui]
+
+
+def merge_dicts(*dict_args):
+    '''
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    '''
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
