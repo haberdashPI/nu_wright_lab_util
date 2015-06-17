cc7d1d428568ca5e48477c902d4228a8cf20bf08
David Little <david.frank.little@gmail.com>
2015-06-17 14:14:42 -0500

    Added function to redirect output, even for c programs.

--- src/misc.py
+++ src/misc.py
@@ -1,4 +1,7 @@
 import numpy as np
+from contextlib import contextmanager
+import os
+import sys
 
 
 def unique_rows(a):
@@ -19,3 +22,34 @@ def merge_dicts(*dict_args):
     for dictionary in dict_args:
         result.update(dictionary)
     return result
+
+
+@contextmanager
+def stdout_redirect(to=os.devnull,mode='w'):
+    '''
+    import os
+
+    with stdout_redirected(to=filename):
+        print("from Python")
+        os.system("echo non-Python applications are also supported")
+    '''
+    fd = sys.stdout.fileno()
+
+    # assert that Python and C stdio write using the same file descriptor
+    # assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
+
+    def _redirect_stdout(to):
+        sys.stdout.close()   # + implicit flush()
+        os.dup2(to.fileno(), fd)   # fd writes to 'to' file
+        sys.stdout = os.fdopen(fd, mode)   # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, mode) as file:
+            _redirect_stdout(to=file)
+        try:
+            yield   # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout.
+            # buffering and flags such as
+            # CLOEXEC may be different
+            _redirect_stdout(to=old_stdout)
---
+++ src/rio.py
@@ -0,0 +1,33 @@
+from contextlib import contextmanager
+import os
+import sys
+
+@contextmanager
+def stdout_redirect(to=os.devnull,mode='w'):
+    '''
+    import os
+
+    with stdout_redirected(to=filename):
+        print("from Python")
+        os.system("echo non-Python applications are also supported")
+    '''
+    fd = sys.stdout.fileno()
+
+    ##### assert that Python and C stdio write using the same file descriptor
+    ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
+
+    def _redirect_stdout(to):
+        sys.stdout.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stdout = os.fdopen(fd, mode) # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, mode) as file:
+            _redirect_stdout(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            _redirect_stdout(to=old_stdout) # restore stdout.
+                                            # buffering and flags such as
+                                            # CLOEXEC may be different
+
