a78250c18b5dfdc68390fd2bdecaf13b760ea438
David Little <david.frank.little@gmail.com>
2015-07-10 12:19:03 -0500

    Added new slope calculation that handles NaN values.

--- src/adjust.py
+++ src/adjust.py
@@ -1,9 +1,15 @@
 from statsmodels.formula.api import ols
 
+
+def find_slope(df,y,covariate):
+    if (df[covariate].isnull().all() or
+        df[y].isnull().all()): return 0
+    else: return ols(y+' ~ '+covariate,df).fit().params[1]
+
+
 def covariate_adjust(df,x,y,covariate,by=[],prefix='adj_'):
     all_by = by + [x]
-    slopes = df.groupby(all_by).\
-          apply(lambda x: ols(y+' ~ '+covariate,x).fit().params[1])
+    slopes = df.groupby(all_by).apply(lambda df: find_slope(df,y,covariate))
     slopes.name = prefix + 'slope_' + y
     df = df.join(slopes,on=all_by)
 
